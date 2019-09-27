4408c96bb459982875c27a3ef215415b10040e93
Sebastian Gaviria Giraldo <1112478378@academico.icesi.edu.co>
2019-09-27 08:20:44 -0500

    rutas dinámicas y formulario con post

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // importar módulo
 const express = require('express');
+// importar body parser
+var bodyParser = require('body-parser');
+// importar file system
+var fs = require('fs');
 
 // instanciar app
 const app = express();
+
+// configuración body parser para poder usar variables post en el body
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // definir puerto
 const port = 3000;
@@ -21,12 +28,33 @@ app.get('/contacto', (request, response) => {
     response.sendFile(__dirname + '/public/contact.html');
 });
 
-app.get('/receiveContact', (request, response) => {
-    console.log('contacto recibido', request.query);
-    response.send('hola');
+app.get('/producto/:name', (request, response) => {
+    response.send('página de producto ' + request.params.name);
+});
+
+app.post('/receiveContact', (request, response) => {
+    console.log('contacto recibido', request.body);
+
+    var data = `
+    Nombre: ${request.body.name}
+    Mensaje: ${request.body.message}
+
+    ----------------------------------
+    `;
+
+    fs.appendFile('messages.txt', data, 'utf8', (err) => {
+        console.log(err ? 'hubo un error' : 'se creó el archivo');
+    });
+
+    response.redirect('/contacto?sent=true');
 });
 
 // inicar servidor en el puerto definido anteriormente
 app.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`);
 });
+
+// get - pedir información al servidor
+// post - enviar nueva información al servidor
+// delete - borrar información del servidor
+// put/patch - actualizar información en el servidor
